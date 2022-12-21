404dc87bbd62f8397747432ba2c15e439035ccec
Clemence <penin.clemence@gmail.com>
2022-12-21 20:23:25 +0100

    creation des animations texte+chevron

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
-import {BsChevronCompactDown} from 'react-icons/bs'
+import { BsChevronCompactDown } from 'react-icons/bs'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,19 +18,41 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.description}>
-          <span>see in </span><span>fr</span><span> | </span><span>en</span>
+        <div className={styles.choice}>
+          <span>see in</span><span>fr</span><span>|</span><span>en</span>
         </div>
 
         <div className={styles.center}>
           <div id="landing">
-            <h1 className={styles.title}><strong>Clémence Guelque</strong><br/>Développeuse Web Fullstack</h1>
-            <h2 className="">         
-            </h2>
-            <BsChevronCompactDown/>
+            <h1 className={styles.title}><strong>Clémence Guelque</strong><br />Développeuse Web Fullstack</h1>
+            <div className="listWrapper">
+              <div className="listRow">
+                
+                <ul class="list">
+                  <li class="listItem">Créons</li>
+                  <li class="listItem">Développons</li>
+                  <li class="listItem">Rions</li>
+                  <li class="listItem">Travaillons</li>
+                </ul>                
+              </div>
+              <span>ensemble</span>
+            </div>
+
+            <BsChevronCompactDown className='chevronDown' href='#projects' />
+          </div>
+
+          <div id="projects">
+
+
+          </div>
+
+          <div id="contact">
+            {/* <Contact/> */}
           </div>
         </div>
-      </main> 
+
+        {/* <Footer/> */}
+      </main>
     </>
   )
 }
