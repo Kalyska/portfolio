57a0f430c61df68d335e9e18652a54fbd8e45f28
Clemence <penin.clemence@gmail.com>
2022-12-22 10:31:46 +0100

    mise en page de la rubrique contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
 
-const Contact=()=>{
+const Contact = () => {
   return (
     <div id='contact'>
-        <h3>Contact</h3>
-        Vous pouvez me contacter via LinkedIn ou email.
-        {/* ajouter icon et lien linkedin */}
-        <strong>Envoyer un email</strong>
-        <form action="">
+      <h3>Contact</h3><hr />
+      <p>Vous pouvez me contacter via LinkedIn <a href='https://linkedin.com/in/clemence-guelque'><BsLinkedin /></a> ou par email :</p>
+      <form action="">
+        <div className="flex">
+          <div className="row">
             <label for="inputName">Nom</label>
             <input type="text" id="inputName" required></input>
             <label for="inputEmail">Email</label>
             <input type="email" id="inputEmail" required></input>
+          </div>
+          <div className="row">
             <label for="textArea">Message</label>
-            <textarea id="textArea" required></textarea>
-            <button type="submit">Envoyer</button>
-        </form>
+            <textarea id="textArea" required rows="5"></textarea>
+          </div>
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
     </div>
   )
 }
