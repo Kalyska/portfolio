78ed6fb79efedae27a7cfb4af9ce23fddf27087e
Clemence <penin.clemence@gmail.com>
2022-12-22 14:25:05 +0100

    mise en route du formulaire de contact via Email JS

--- components/Contact.js
+++ components/Contact.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import {BsLinkedin} from 'react-icons/bs';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div id='contact'>
       <h3>Contact</h3><hr />
       <p>Vous pouvez me contacter via LinkedIn <a href='https://linkedin.com/in/clemence-guelque'><BsLinkedin /></a> ou par email :</p>
-      <form action="">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="flex">
           <div className="row">
             <label for="inputName">Nom</label>
-            <input type="text" id="inputName" required></input>
+            <input type="text" id="inputName" name="user_name" required></input>
             <label for="inputEmail">Email</label>
-            <input type="email" id="inputEmail" required></input>
+            <input type="email" id="inputEmail" name="user_email" required></input>
           </div>
           <div className="row">
             <label for="textArea">Message</label>
-            <textarea id="textArea" required rows="5"></textarea>
+            <textarea id="textArea" name="message" required rows="5"></textarea>
           </div>
         </div>
-        <button type="submit">Envoyer</button>
+        <button type="submit" value="Send">Envoyer</button>
       </form>
     </div>
   )
