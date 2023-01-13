dd6ab0f58af16ba1a3778df522277947f979d7ca
Clemence <penin.clemence@gmail.com>
2023-01-13 10:09:16 +0100

    ajout du cursor pointer sur choix des langues + images du carousel

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const Header=()=>{
-  const{t, i18n}=useTranslation();
-  function handleLangChange(e){
+const Header = () => {
+  const { t, i18n } = useTranslation();
+  function handleLangChange(e) {
     i18n.changeLanguage(e.target.className)
   };
-  
+
   return (
     <header>
-        <span>see in</span><span onClick={handleLangChange} className='fr'>fr</span><span>|</span><span onClick={handleLangChange} className='en'>en</span>
+      <p>see in <span onClick={handleLangChange} className='fr'>fr</span> | <span onClick={handleLangChange} className='en'>en</span></p>
     </header>
   )
 }
