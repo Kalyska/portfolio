7540f5b29576c0d8d74252bb20da24626c23a82b
Clemence <penin.clemence@gmail.com>
2023-02-01 20:46:42 +0100

    changement ordre affichage slides carrousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useRouter} from'next/router';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 
 // Import Swiper React components
@@ -13,10 +13,10 @@ import "swiper/scss/navigation";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-export const Carousel=()=>  {
+export const Carousel = () => {
 
-  const router= useRouter();
-  const handleClick= (project=> router.push(`/project/${project}`))
+  const router = useRouter();
+  const handleClick = (project => router.push(`/project/${project}`))
 
   return (
     <>
@@ -34,10 +34,10 @@ export const Carousel=()=>  {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
+        <SwiperSlide><Image className='img' src="/img/movieshuffle1.png" alt="" fill object-fit='contain' onClick={() => handleClick("movieshuffle")} /></SwiperSlide>
         <SwiperSlide><Image className='img' src="/img/wizard1.png" alt="" fill object-fit='contain' onClick={() => handleClick("wizard")} /></SwiperSlide>
-        <SwiperSlide><Image className='img' src="/img/drink1.png" alt="" fill object-fit='contain' onClick={() => handleClick("drink")}/></SwiperSlide>
-        <SwiperSlide><Image className='img' src="/img/cake1.png" alt="" fill object-fit='contain' onClick={() => handleClick("cake")}/></SwiperSlide>
-        <SwiperSlide><Image className='img' src="/img/movieshuffle1.png" alt="" fill object-fit='contain' onClick={() => handleClick("movieshuffle")}/></SwiperSlide>
+        <SwiperSlide><Image className='img' src="/img/drink1.png" alt="" fill object-fit='contain' onClick={() => handleClick("drink")} /></SwiperSlide>
+        <SwiperSlide><Image className='img' src="/img/cake1.png" alt="" fill object-fit='contain' onClick={() => handleClick("cake")} /></SwiperSlide>
       </Swiper>
     </>
   );
